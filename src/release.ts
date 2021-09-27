25d5e2609c5c7e160a25598a9a194cc0f460e2d4
Yusuke Sakurai <kerokerokerop@gmail.com>
2021-09-27 17:49:04 +0900

    use actions/exec

--- src/release.ts
+++ src/release.ts
@@ -1,28 +1,6 @@
 import { getOctokit } from "@actions/github";
+import { getExecOutput } from "@actions/exec";
 import * as semver from "semver";
-import * as cp from "child_process";
-
-function getExecOutput(cmd: string, args: string[]) {
-  const p = cp.spawn(cmd, args);
-  console.log(cmd, ...args);
-  return new Promise<{
-    exitCode: number;
-    stdout: string;
-    stderr: string;
-  }>((resolve) => {
-    let stdout: string = "";
-    let stderr: string = "";
-    p.stdout.on("data", (d) => (stdout += d));
-    p.stderr.on("data", (d) => (stderr += d));
-    p.on("exit", (code) =>
-      resolve({
-        exitCode: code ?? 1,
-        stdout: stdout,
-        stderr: stderr,
-      })
-    );
-  });
-}
 
 async function getLogs(tag: string, toTag: string | undefined) {
   const args = ["log", "--oneline"];
@@ -44,7 +22,6 @@ async function getLatestTag(p: {
     throw new Error(output.stderr);
   }
   let list = output.stdout.split("\n").filter((v) => semver.valid(v));
-  console.log(list);
   if (!p.includeRc) {
     list = list.filter((v) => !!v.match(/^v?(\d+)\.(\d+)\.(\d+)$/));
   }
@@ -69,7 +46,6 @@ export async function createRelease({
   const latest = await getLatestTag({ includeRc: !!tag.match(/-rc\d+$/) });
   let body = await getLogs(tag, latest);
   body = body.replace("\n", "<br />");
-  console.log(body);
   const github = getOctokit(token);
   let release_id: number | undefined;
   try {
